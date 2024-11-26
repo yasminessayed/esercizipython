b7873839689eaf6dff7744b8b7e27f3e68b4f515
yasminessayed <yasminessayed54@gmail.com>
2024-11-26 13:13:51 +0100

    .gitignore

---
+++ esercizio_commenti.py
@@ -0,0 +1,15 @@
+"""
+print ("IL MIO CONTO")
+nome = input("Entra UserName:\n")
+password = input("Entra Password:\n")
+conto =input("1000")
+prelievo=0
+deposito=0
+if(conto>0):
+    prelievo=1000+100
+    print("il prelievo è valido")
+else:
+    print("non è valido")
+
+ """
+#
